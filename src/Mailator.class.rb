6ce77bd4359e6bff5ce924fcbf0e1c17f5481dc4
Syscall <69993119+Syscall0336@users.noreply.github.com>
2020-10-09 22:54:19 +0200

    Update Mailator.class.rb

--- src/Mailator.class.rb
+++ src/Mailator.class.rb
@@ -1,23 +1,3 @@
-=begin
-
-msgstr = <<END_OF_MESSAGE
-From: Your Name <your@mail.address>
-To: Destination Address <someone@example.com>
-Subject: test message
-Date: Sat, 23 Jun 2001 16:26:43 +0900
-Message-Id: <unique.message.id.string@example.com>
-
-This is a test message.
-END_OF_MESSAGE
-
-require 'net/smtp'
-Net::SMTP.start('your.smtp.server', 25) do |smtp|
-  smtp.send_message msgstr,
-                    'your@mail.address',
-                    'his_address@example.com'
-end
-
-=end
 module Mailator
 
     class Converter
